b7968679783a2a29d8f0372b945fd9f5de1cb2a6
alexander <orlov.03.05.2004@mail.ru>
2022-02-28 17:34:42 +0300

    fix: add check for null object in locastorage

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,33 +5,8 @@ import '../Header/Header.css'
 import { NavLink } from 'react-router-dom';
 
 const Navigation = React.memo((props) =>{
-    const[isOpen, setOpen] = React.useState(false);
-    React.useEffect(() => {
-        window.addEventListener('resize', () => {
-            if(window.innerWidth > 768 ){
-                setOpen(false);
-            } 
-            if(window.innerWidth <= 768 ){
-                setOpen(props.isOpen);
-            } 
-        })
-        return () => {
-            window.removeEventListener('resize', () => {
-                if(window.innerWidth > 768 ){
-                    setOpen(false);
-                } 
-                if(window.innerWidth <= 768 ){
-                    setOpen(props.isOpen);
-                } 
-            })
-        }
-    }, [])
-    React.useEffect(() => {
-        setOpen(props.isOpen);
-        console.log(props.isOpen)
-    }, [props.isOpen])
     return(
-        <Popup isOpen={window.innerWidth > 768 ? false : isOpen} onClose={props.closePopup}>
+        <Popup isOpen={window.innerWidth > 768 ? false : props.isOpen} onClose={props.closePopup}>
             <nav className="navigation">
                 <NavLink className="header__link-account" activeClassName="header__link_active" to='/profile' onClick={props.closePopup}>Аккаунт
                     <div className="header__icon-account"></div>
